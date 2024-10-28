d24f470a01eaae40622ee39da8aa607bb1b34d48
mgading13 <muhammadgading43@gmail.com>
2024-10-28 22:07:00 +0800

    modify task.js

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const url = "https://fs1076432-react-exercise-netlify.netlify.app/" // TODO: replace this
+const studentName = "Muhammad Gading" // TODO: replace this
+const studentId = "FS10764322" // TODO: replace this
 
 export { url, studentName, studentId }
